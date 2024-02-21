eb337f6004a04390afda2d6ab8c12968ccdcc797
wahyu2078 <wahyutptp20@gmail.com>
2024-02-21 21:06:37 +0700

    jobsheet1

--- Array29.java
+++ Array29.java
@@ -4,7 +4,7 @@ public class Array29 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Menghitung IP Semester");
+        System.out.println("Program Menghitung IP Semester");
 
         String[] mataKuliah = {
             "Pancasila", "Konsep Teknologi Informasi", "Critical Thinking dan Problem Solving ",
@@ -14,24 +14,24 @@ public class Array29 {
 
         double[] nilaiAngka = new double[mataKuliah.length];
         double totalBobot = 0;
-        int totalKredit = 0;
-        int[] kredit = { 2, 3, 3, 3, 3, 3, 1, 2 };
+        int totalBobotMataKuliah = 0;
+        int[] bobot = { 2, 3, 3, 3, 3, 3, 1, 2 };
 
         for (int i = 0; i < mataKuliah.length; i++) {
             System.out.print("Masukkan nilai Angka untuk MK " + mataKuliah[i] + ": ");
             nilaiAngka[i] = scanner.nextDouble();
 
-            double bobot;
-            if (nilaiAngka[i] >= 80) bobot = 4.00;
-            else if (nilaiAngka[i] >= 73) bobot = 3.50;
-            else if (nilaiAngka[i] >= 65) bobot = 3.00;
-            else if (nilaiAngka[i] >= 60) bobot = 2.50;
-            else if (nilaiAngka[i] >= 50) bobot = 2.00;
-            else if (nilaiAngka[i] >= 39) bobot = 1.00;
-            else bobot = 0.00;
+            double nilaiBobot;
+            if (nilaiAngka[i] >= 80) nilaiBobot = 4.00;
+            else if (nilaiAngka[i] >= 73) nilaiBobot = 3.50;
+            else if (nilaiAngka[i] >= 65) nilaiBobot = 3.00;
+            else if (nilaiAngka[i] >= 60) nilaiBobot = 2.50;
+            else if (nilaiAngka[i] >= 50) nilaiBobot = 2.00;
+            else if (nilaiAngka[i] >= 39) nilaiBobot = 1.00;
+            else nilaiBobot = 0.00;
 
-            totalBobot += bobot * kredit[i]; 
-            totalKredit += kredit[i];
+            totalBobot += nilaiBobot * bobot[i]; 
+            totalBobotMataKuliah += bobot[i]; 
 
             String nilaiHuruf;
             if (nilaiAngka[i] >= 80) nilaiHuruf = "A";
@@ -42,10 +42,10 @@ public class Array29 {
             else if (nilaiAngka[i] >= 39) nilaiHuruf = "D";
             else nilaiHuruf = "E";
 
-            System.out.printf("%-30s%-15.2f%-15s%-10.2f\n", mataKuliah[i], nilaiAngka[i], nilaiHuruf, bobot);
+            System.out.printf("%-30s%-15.2f%-15s%-10.2f\n", mataKuliah[i], nilaiAngka[i], nilaiHuruf, nilaiBobot);
         }
 
-        double ip = totalBobot / totalKredit;
+        double ip = totalBobot / totalBobotMataKuliah;
         System.out.println("IP : " + String.format("%.2f", ip));
     }
 }
